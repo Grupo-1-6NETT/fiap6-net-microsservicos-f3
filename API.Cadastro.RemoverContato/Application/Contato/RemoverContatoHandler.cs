566464178edb31c9a535a77ee0b12e953b91cc2c
diego <diego.feijo@hotmail.com>
2025-03-21 20:40:07 -0300

    feature: adiciona API.Cadastro.RemoverContatos.sln

---
+++ API.Cadastro.RemoverContato/Application/Contato/RemoverContatoHandler.cs
@@ -0,0 +1,14 @@
+﻿using Application.Services;
+using MediatR;
+
+namespace Application.Contato;
+
+public class RemoverContatoHandler(IRabbitMQService rabbitmq) : IRequestHandler<RemoverContatoCommand, Guid>
+{
+    public async Task<Guid> Handle(RemoverContatoCommand request, CancellationToken cancellationToken)
+    {
+        await rabbitmq.PublicarMensagem(request);
+
+        return request.Id;
+    }
+}
