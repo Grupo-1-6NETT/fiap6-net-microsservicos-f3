16a33c33e851116606ba3f9a5af5dc1e1c22cec3
diego <diego.feijo@hotmail.com>
2025-03-22 19:34:05 -0300

    feature: uso de MassTransit para leitura de fila add: AdicionarContatoDto.cs para transporte dos dados de Contato

--- Microservice.Cadastro.AdicionarContato/Consumer/Program.cs
+++ Microservice.Cadastro.AdicionarContato/Consumer/Program.cs
@@ -1,12 +1,44 @@
 using Consumer;
+using Consumer.Eventos;
 using ContatoDb.Core;
+using MassTransit;
 
 var builder = Host.CreateApplicationBuilder(args);
 builder.Services.AddHostedService<Worker>();
 
-var sqliteConnString = builder.Configuration.GetSection("SQLiteConnection").Value;
+var configuration = builder.Configuration;
 
+var sqliteConnString = configuration.GetSection("SQLiteConnection").Value;
 builder.Services.AddContatoDbCoreServices(sqliteConnString);
+
+
+var fila = configuration.GetSection("RabbitMQ")["QueueName"] ?? string.Empty;
+var servidor = configuration.GetSection("RabbitMQ")["Hostname"] ?? string.Empty;
+var usuario = configuration.GetSection("RabbitMQ")["Username"] ?? string.Empty;
+var senha = configuration.GetSection("RabbitMQ")["Password"] ?? string.Empty;
+
+builder.Services.AddMassTransit(x =>
+{
+    x.UsingRabbitMq((context, cfg) =>
+    {
+        cfg.Host(servidor, "/", h =>
+        {
+            h.Username(usuario);
+            h.Password(senha);
+        });
+
+        cfg.ReceiveEndpoint(fila, e =>
+        {
+            e.ConfigureConsumer<CriarContatoConsumidor>(context);
+        });
+
+        cfg.ConfigureEndpoints(context);
+    });
+
+    x.AddConsumer<CriarContatoConsumidor>();
+});
+
+
 
 var host = builder.Build();
 host.Run();
--- Microservice.Cadastro.AdicionarContato/Consumer/Worker.cs
+++ Microservice.Cadastro.AdicionarContato/Consumer/Worker.cs
@@ -1,87 +1,24 @@
-using RabbitMQ.Client;
-using RabbitMQ.Client.Events;
-using System.Text;
-using System.Text.Json;
-using ContatoDb.Core.Interfaces;
-using ContatoDb.Core.Models;
-
 namespace Consumer;
+
 public class Worker : BackgroundService
 {
     private readonly ILogger<Worker> _logger;
-    private readonly IConnectionFactory _factory;
-    private readonly string _queueName;
-    private IChannel _channel;
-    private IConnection _connection;
-    private IContatoRepository _contatoRepository;
 
-    public Worker(IConfiguration config, ILogger<Worker> logger)
+    public Worker(ILogger<Worker> logger)
     {
-        //_contatoRepository = contatoRepository;
         _logger = logger;
-        _factory = new ConnectionFactory()
-        {
-            HostName = config["RabbitMQ:Hostname"] ?? string.Empty,
-            UserName = config["RabbitMQ:Username"] ?? string.Empty,
-            Password = config["RabbitMQ:Password"] ?? string.Empty,
-        };
-        _queueName = config["RabbitMQ:QueueName"] ?? string.Empty;
-
-        Setup().Wait();
-    }
-    
-    public override async Task StopAsync(CancellationToken stoppingToken)
-    {
-        _logger.LogInformation("Finalizando o Consumer...");
-
-        _channel?.Dispose();
-        _connection?.Dispose();
-
-        _logger.LogInformation("Conexão fechada.");
-
-        await base.StopAsync(stoppingToken);
-    }
-
-    private async Task Setup()
-    {
-        _connection = await _factory.CreateConnectionAsync();
-        _channel = await _connection.CreateChannelAsync();
-
-        await _channel.QueueDeclareAsync(
-            queue: _queueName,
-            durable: true,
-            exclusive: false,
-            autoDelete: false
-        );
     }
 
     protected override async Task ExecuteAsync(CancellationToken stoppingToken)
     {
-        var consumer = new AsyncEventingBasicConsumer(_channel);
-        _logger.LogInformation("Consumer iniciado. Aguardando mensagens...");
+        while (!stoppingToken.IsCancellationRequested)
+        {
+            if (_logger.IsEnabled(LogLevel.Information))
+            {
+                _logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
+            }
 
-        consumer.ReceivedAsync += Contato_Recebido;
-
-        await _channel.BasicConsumeAsync(
-                queue: _queueName,
-                autoAck: false,
-                consumer: consumer, 
-                cancellationToken: stoppingToken);
-
-        await Task.Delay(Timeout.Infinite, stoppingToken);
-    }
-
-    private async Task Contato_Recebido(object sender, BasicDeliverEventArgs args)
-    {
-        var body = args.Body.ToArray();
-        var message = Encoding.UTF8.GetString(body);
-        var contato = JsonSerializer.Deserialize<Contato>(message);
-
-        if (contato != null) 
-            await _contatoRepository.CreateAsync(contato);
-
-        await _channel.BasicAckAsync(deliveryTag: args.DeliveryTag, multiple: false, cancellationToken: args.CancellationToken);
-
-        await Task.CompletedTask;
+            await Task.Delay(1000, stoppingToken);
+        }
     }
 }
