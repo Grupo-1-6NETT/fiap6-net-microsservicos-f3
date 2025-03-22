3f68608c2fd03a0084911ba094a4fefb8ac32fab
Felipe Dantas <felipemenezesdantas@gmail.com>
2025-03-22 00:39:33 -0300

    fixed: docker file auth

--- Microservice.Cadastro.Gateway/src/Cadastro.Gateway.API/Program.cs
+++ Microservice.Cadastro.Gateway/src/Cadastro.Gateway.API/Program.cs
@@ -3,6 +3,7 @@ using Microsoft.IdentityModel.Tokens;
 using Microsoft.OpenApi.Models;
 using Ocelot.DependencyInjection;
 using Ocelot.Middleware;
+using Prometheus;
 using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -38,36 +39,14 @@ builder.Services.AddSwaggerGen(options =>
     {
         Title = "Gateway API",
         Version = "v1",
-        Description = "API para Gateway e Auth",
-    });
-
-    options.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
-    {
-        Description = "Insira o token JWT (com prefixo Bearer) abaixo. Exemplo: 'Bearer meuToken123'",
-        Name = "Authorization",
-        In = ParameterLocation.Header,
-        Type = SecuritySchemeType.Http,
-        Scheme = "Bearer",
-        BearerFormat = "JWT",
-    });
-
-    options.AddSecurityRequirement(new OpenApiSecurityRequirement
-    {
-        {
-            new OpenApiSecurityScheme
-            {
-                Reference = new OpenApiReference
-                {
-                    Type = ReferenceType.SecurityScheme,
-                    Id = "Bearer"
-                }
-            },
-            Array.Empty<string>()
-        }
+        Description = "API para Gateway",
     });
 });
 
 var app = builder.Build();
+
+app.UseMetricServer();
+app.UseHttpMetrics();
 
 if (app.Environment.IsDevelopment())
 {
@@ -75,9 +54,17 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
+app.UseSwagger();
+app.UseSwaggerUI(c =>
+{
+    c.SwaggerEndpoint("/swagger/v1/swagger.json", "Gateway API V1");
+});
+
 app.UseRouting();
 app.UseAuthentication();
 app.UseAuthorization();
+
+app.MapMetrics();
 app.UseOcelot().Wait();
 
 app.Run();
