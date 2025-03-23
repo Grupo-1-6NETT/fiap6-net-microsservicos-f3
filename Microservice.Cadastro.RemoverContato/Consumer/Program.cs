479e53e30164426e9694ee9a238b3f3da9e3fd72
diego <diego.feijo@hotmail.com>
2025-03-23 12:55:15 -0300

    fix: ajusta Microservice.Cadastro.RemoverContato.sln para usar MassTransit

---
+++ Microservice.Cadastro.RemoverContato/Consumer/Program.cs
@@ -0,0 +1,43 @@
+using Consumer;
+using Consumer.Eventos;
+using ContatoDb.Core;
+using MassTransit;
+
+var builder = Host.CreateApplicationBuilder(args);
+builder.Services.AddHostedService<Worker>();
+
+
+var configuration = builder.Configuration;
+
+var sqliteConnString = configuration.GetSection("SQLiteConnection").Value;
+builder.Services.AddContatoDbCoreServices(sqliteConnString);
+
+
+var fila = configuration.GetSection("RabbitMQ")["QueueName"] ?? string.Empty;
+var servidor = configuration.GetSection("RabbitMQ")["Hostname"] ?? string.Empty;
+var usuario = configuration.GetSection("RabbitMQ")["Username"] ?? string.Empty;
+var senha = configuration.GetSection("RabbitMQ")["Password"] ?? string.Empty;
+
+builder.Services.AddMassTransit(x =>
+{
+    x.UsingRabbitMq((context, cfg) =>
+    {
+        cfg.Host(servidor, "/", h =>
+        {
+            h.Username(usuario);
+            h.Password(senha);
+        });
+
+        cfg.ReceiveEndpoint(fila, e =>
+        {
+            e.ConfigureConsumer<RemoverContatoConsumidor>(context);
+        });
+
+        cfg.ConfigureEndpoints(context);
+    });
+
+    x.AddConsumer<RemoverContatoConsumidor>();
+});
+
+var host = builder.Build();
+host.Run();
