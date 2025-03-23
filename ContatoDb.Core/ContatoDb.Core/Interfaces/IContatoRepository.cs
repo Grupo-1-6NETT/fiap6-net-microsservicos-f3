9e3c463742e9f23f39b8528d46dfdd3ccfba933c
diego <diego.feijo@hotmail.com>
2025-03-23 12:56:36 -0300

    adds: métodos read, update e delete adicionados ao repositório

--- ContatoDb.Core/ContatoDb.Core/Interfaces/IContatoRepository.cs
+++ ContatoDb.Core/ContatoDb.Core/Interfaces/IContatoRepository.cs
@@ -4,6 +4,10 @@ namespace ContatoDb.Core.Interfaces;
 
 public interface IContatoRepository
 {    
-    Task<Contato?> GetByIdAsync(Guid id);
     Task CreateAsync(Contato contato);
+    Task<Contato?> GetByIdAsync(Guid id);
+    Task<IEnumerable<Contato>> GetAllAsync(int? pageIndex, int? pageSize);
+    Task<IEnumerable<Contato>> GetByDddAsync(string ddd, int? pageIndex, int? pageSize);
+    void Update(Contato contato);
+    Task DeleteAsync(Guid id);
 }
--- ContatoDb.Core/ContatoDb.Core/Repository/ContatoRepository.cs
+++ ContatoDb.Core/ContatoDb.Core/Repository/ContatoRepository.cs
@@ -1,19 +1,68 @@
 using ContatoDb.Core.Data;
 using ContatoDb.Core.Interfaces;
 using ContatoDb.Core.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace ContatoDb.Core.Repository;
 
 public class ContatoRepository(AppDbContext context) : IContatoRepository
 {
+    public async Task CreateAsync(Contato contato)
+    {
+        await context.Set<Contato>().AddAsync(contato);
+        await context.SaveChangesAsync();
+    }
+    
     public async Task<Contato?> GetByIdAsync(Guid id)
     {
         return await context.Set<Contato>().FindAsync(id);
     }
 
-    public async Task CreateAsync(Contato contato)
+    public async Task<IEnumerable<Contato>> GetAllAsync(int? pageIndex, int? pageSize)
     {
-        await context.Set<Contato>().AddAsync(contato);
-        await context.SaveChangesAsync();
+        var query = context.Set<Contato>()
+            .AsNoTracking()            
+            .AsQueryable();
+
+        if (pageIndex is null || pageSize is null)
+            return await query.ToListAsync();
+
+        return await query
+            .Skip((pageIndex.Value - 1) * pageSize.Value)
+            .Take(pageSize.Value)
+            .ToListAsync();
+    }
+
+    public async Task<IEnumerable<Contato>> GetByDddAsync(string ddd, int? pageIndex, int? pageSize)
+    {
+        var query = context.Contatos
+            .AsNoTracking()
+            .Where(c => c.DDD == ddd)
+            .OrderBy(c => c.Nome)
+            .AsQueryable();
+
+        if (pageIndex is null || pageSize is null)
+            return await query.ToListAsync();
+
+        return await query
+            .Skip((pageIndex.Value - 1) * pageSize.Value)
+            .Take(pageSize.Value)
+            .ToListAsync();
+    }
+    
+    public void Update(Contato contato)
+    {
+        context.Set<Contato>().Update(contato);
+        context.SaveChanges();
+    }
+    
+    public async Task DeleteAsync(Guid id)
+    {
+        var contato = await GetByIdAsync(id);
+        if (contato is not null)
+        {
+            context.Set<Contato>().Remove(contato);
+            await context.SaveChangesAsync();
+        }
     }
 }
