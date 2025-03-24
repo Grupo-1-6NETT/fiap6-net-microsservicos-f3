936ca1b8e299492fa20c2d2f4a97628e63f555c0
Diego Feijó <diego.feijo@hotmail.com>
2025-03-24 19:50:14 -0300

    clean up

--- Microservice.Cadastro.Consulta/src/Cadastro.Consulta.Consumer/Models/ConsultaRequest.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Cadastro.Consulta.Consumer.Models;
-public class ConsultaRequest
-{
-    public string? DDD { get; set; }
-    public int? PageIndex { get; set; }
-    public int? PageSize { get; set; }
-}
--- Microservice.CadastroContato/src/Cadastro.Application/Contato/Consultar/ConsultarContatoQuery.cs
+++
@@ -1,9 +0,0 @@
-﻿using MediatR;
-
-namespace Cadastro.Application.Contato.Consultar;
-public class ConsultarContatoQuery : IRequest<IEnumerable<Domain.Models.Contato>>
-{
-    public string? DDD { get; set; }
-    public int? PageIndex { get; set; }
-    public int? PageSize { get; set; }
-}
